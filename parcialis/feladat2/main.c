ac7f4c177662905bb782b54f6e047678d5b933d9
palan <pal.anti84@gmail.com>
2026-03-29 13:35:55 +0300

    elfelejtet commit a feladat2-höz

---
+++ parcialis/feladat2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "functions.h"
+
+/* Milyen sorrendben tehetjük össze a süti hozzávalókat,
+ * hogy a közvetlenül az élesztő előtt és után ne
+ * adjunk hozzá a sót?
+ */
+int main(void) {
+    Hozzavalo hozzavalok[]={cukor, liszt, eleszto, so, tej};
+    int n=sizeof(hozzavalok)/sizeof(hozzavalok[0]);
+    int *x= (int*)calloc(n, sizeof(int));
+    if (!x) {
+        printf("Memory allocation failed\n");
+        exit(-1);
+    }
+    BTP(x,n,0, hozzavalok);
+    free(x);    return 0;
+}
